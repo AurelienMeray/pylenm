dfada1ae19dd917cc13f5ccdf2506904d11c8ec6
AurelienMeray <aurelien.meray@gmail.com>
2020-07-09 15:19:31 -0400

    update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pylenm',
-      version='0.1.1',
+      version='0.1.2',
       description='Python functions for Analyzing Historical Groundwater Datasets',
       url='https://github.com/AurelienMeray/pylenm',
       author='Aurelien Meray',
