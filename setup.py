9235d8f1a7189dacc24f6c1f186f60074fdbde9a
AurelienMeray <aurelien.meray@gmail.com>
2020-07-15 10:04:24 -0400

    restructed code into classes so that importing data is easy and not repetitive for the user.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pylenm',
-      version='0.1.2',
+      version='0.1.3',
       description='Python functions for Analyzing Historical Groundwater Datasets',
       url='https://github.com/AurelienMeray/pylenm',
       author='Aurelien Meray',
