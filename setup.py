75a4c51bcbbc910468a0aacf6354b83b66dcbc00
AurelienMeray <aurelien.meray@gmail.com>
2020-08-10 09:40:00 -0400

    Updated project description and fixed pip install issue.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pylenm',
       version='0.1.5',
-      description='Python functions for Analyzing Historical Groundwater Datasets',
+      description='This package aims to provide machine learning (ML) functions for performing comprehensive soil and groundwater data analysis, and for supporting the establishment of effective long-term monitoring.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/AurelienMeray/pylenm',
       author='Aurelien Meray',
       author_email='aurelien.meray@gmail.com',
       license='MIT',
-      packages=[],
+      packages=['pylenm'],
        install_requires=[
           'markdown',
           'pandas',
