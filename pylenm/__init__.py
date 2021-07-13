9f665fa297d1ba36e1e9699fbb98f025bf581d65
AurelienMeray <aurelien.meray@gmail.com>
2021-07-13 10:23:49 -0400

    unlink

--- pylenm/__init__.py
+++ pylenm/__init__.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 __version__ = '0.1.12.9'
-=======
-__version__ = '0.1.12.6'
->>>>>>> fcb58663518d88e0df29d5f0bc7809c1d3eae624
 
 from .pylenm import functions
 from . import pylenm_usages as usage
