f40e7adfc4ca73aa4c4cc368ed7364c3ef905abe
AurelienMeray <aurelien.meray@gmail.com>
2022-05-31 17:37:51 -0400

    added docstring

--- pylenm/pylenm.py
+++ pylenm/pylenm.py
@@ -1,30 +1,61 @@
-from ._imports import *
-
-class init:
+class ExampleClass(object):
     """The summary line for a class docstring should fit on one line.
 
-        If the class has public attributes, they may be documented here
-        in an ``Attributes`` section and follow the same formatting as a
-        function's ``Args`` section. Alternatively, attributes may be documented
-        inline with the attribute's declaration (see __init__ method below).
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
 
-        Properties created with the ``@property`` decorator should be documented
-        in the property's getter method.
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
 
-        Attributes:
-            attr1 (str): Description of `attr1`.
-            attr2 (:obj:`int`, optional): Description of `attr2`.
+    Attributes:
+        attr1 (str): Description of `attr1`.
+        attr2 (:obj:`int`, optional): Description of `attr2`.
 
     """
-    
-    def __init__(self, data: pd.DataFrame):
+
+    def __init__(self, param1, param2, param3):
         """Example of docstring on the __init__ method.
 
         The __init__ method may be documented in either the class level
         docstring, or as a docstring on the __init__ method itself.
 
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
         Args:
-            data (pd.DataFrame): Data to be imported.
+            param1 (str): Description of `param1`.
+            param2 (:obj:`int`, optional): Description of `param2`. Multiple
+                lines are supported.
+            param3 (:obj:`list` of :obj:`str`): Description of `param3`.
+
         """
-        self.setData(data)
-        self.__jointData = [None, 0]
+        self.attr1 = param1
+        self.attr2 = param2
+        self.attr3 = param3  #: Doc comment *inline* with attribute
+
+        #: list of str: Doc comment *before* attribute, with type specified
+        self.attr4 = ['attr4']
+
+        self.attr5 = None
+        """str: Docstring *after* attribute, with type specified."""
+
+    def example_method(self, param1, param2):
+        """Class methods are similar to regular functions.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Args:
+            param1: The first parameter.
+            param2: The second parameter.
+
+        Returns:
+            True if successful, False otherwise.
+
+        """
+        return True
