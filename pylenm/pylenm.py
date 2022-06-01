a2c504df4c242f06e62bc742f1814cc8211ff1f9
AurelienMeray <aurelien.meray@gmail.com>
2022-06-01 14:35:49 -0400

    added imports to file

--- pylenm/pylenm.py
+++ pylenm/pylenm.py
@@ -15,28 +15,28 @@ from math import sqrt
 import matplotlib.dates as mdates
 from matplotlib.dates import date2num, num2date
 from sklearn import preprocessing
-# from scipy.optimize import curve_fit
-# from supersmoother import SuperSmoother
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.decomposition import PCA
-# from sklearn.cluster import KMeans
-# from sklearn.gaussian_process import GaussianProcessRegressor
-# from sklearn.ensemble import RandomForestRegressor
-# from sklearn.pipeline import make_pipeline
-# from sklearn.gaussian_process.kernels import Matern, WhiteKernel
-# from sklearn.preprocessing import PolynomialFeatures
-# from sklearn.linear_model import LinearRegression, Ridge, Lasso, RidgeCV, LassoCV
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.metrics import mean_squared_error
-# import scipy.stats as stats
-# import warnings
-# # warnings.filterwarnings("ignore")
-# from pyproj import Proj, Transformer
-# from ipyleaflet import (Map, basemaps, WidgetControl, GeoJSON, 
-#                         LayersControl, Icon, Marker,FullScreenControl,
-#                         CircleMarker, Popup, AwesomeIcon) 
-# from ipywidgets import HTML
-# # plt.rcParams["font.family"] = "Times New Roman"
+from scipy.optimize import curve_fit
+from supersmoother import SuperSmoother
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.gaussian_process.kernels import Matern, WhiteKernel
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, RidgeCV, LassoCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+import scipy.stats as stats
+import warnings
+# warnings.filterwarnings("ignore")
+from pyproj import Proj, Transformer
+from ipyleaflet import (Map, basemaps, WidgetControl, GeoJSON, 
+                        LayersControl, Icon, Marker,FullScreenControl,
+                        CircleMarker, Popup, AwesomeIcon) 
+from ipywidgets import HTML
+# plt.rcParams["font.family"] = "Times New Roman"
 
 class initialize_pylenm(object):
     """The summary line for a class docstring should fit on one line.
