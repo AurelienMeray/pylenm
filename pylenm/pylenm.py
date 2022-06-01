e639243e85d9701ff503ecbecfd6d7798e2e6ab6
AurelienMeray <aurelien.meray@gmail.com>
2022-06-01 11:37:00 -0400

    updated again pylenm.py

--- pylenm/pylenm.py
+++ pylenm/pylenm.py
@@ -11,31 +11,5 @@ class init(object):
     Properties created with the ``@property`` decorator should be documented
     in the property's getter method.
 
-    Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
 
     """
-    
-    def __init__(self, data: pd.DataFrame):
-        """Example of docstring on the __init__ method.
-
-        The __init__ method may be documented in either the class level
-        docstring, or as a docstring on the __init__ method itself.
-
-        Either form is acceptable, but the two should not be mixed. Choose one
-        convention to document the __init__ method and be consistent with it.
-
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-
-        Args:
-            param1 (str): Description of `param1`.
-            param2 (:obj:`int`, optional): Description of `param2`. Multiple
-                lines are supported.
-            param3 (:obj:`list` of :obj:`str`): Description of `param3`.
-
-        """
-        self.setData(data)
-        self.__jointData = [None, 0]
-
